dcfeb2f2e000bb77f37b7c0e7144627c0460d5e7
minfive <chenxiaowu1994@outlook.com>
2017-09-07 00:21:40 +0800

    build(release): update release script

--- build/release.js
+++ build/release.js
@@ -7,6 +7,7 @@
  */
 
 const exec = require('child_process').exec;
+const standardVersion = require('standard-version');
 
 function getBranchName() {
     return new Promise((resolve, reject) => {
@@ -19,3 +20,32 @@ function getBranchName() {
         });
     });
 }
+
+function updateChangelog() {
+    standardVersion(
+        {
+            noVerify: true,
+            infile: 'CHANGELOG.md',
+            silent: false,
+            skip: {
+                tag: false
+            }
+        },
+        funcion(err) {
+            if (err) {
+                throw err;
+            }
+        }
+    );
+}
+
+async function release() {
+    if (await getBranchName() === 'master') {
+        console.log('合并到 master 分支');
+        updateChangelog();
+    } else {
+        console.log('合并了');
+    }
+}
+
+release();
